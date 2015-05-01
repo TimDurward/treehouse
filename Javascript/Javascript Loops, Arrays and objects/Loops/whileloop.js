054d3611d0c05ef72d5c8e4b59193a97215c3481
Tim Durward <Timothy.A.Durward@gmail.com>
2015-04-30 20:01:57 -0700

    Changed folder dir
    Deleted unwanted folders and empty folders. Organization.

---
+++ Javascript/Javascript Loops, Arrays and objects/Loops/whileloop.js
@@ -0,0 +1,51 @@
+// Finally understand do..while and while loops. Omg that was ridiculous. 
+// Moving on to For...loops. Congrats to myself. This is fantastic. I'm so happy.
+
+
+// Counting Variable/break
+var counter = 1;
+
+
+//WHILE LOOPS
+//while (counter <= 9) {
+//	if (counter.toString().length === 1){
+//	document.write("<p>" + "{ " + counter + " Million }");
+//} else{
+//	document.write(counter+" | ");
+//}
+//	counter += 1;
+//}
+
+
+
+//DO WHILE LOOPS
+do {
+ {
+	if (counter.toString().length === 1){
+	document.write("<p>" + "{ " + counter + " Million }");
+} else{
+	document.write(counter+" | ");
+}
+	counter += 1;
+}
+
+} while (counter <= 9) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
