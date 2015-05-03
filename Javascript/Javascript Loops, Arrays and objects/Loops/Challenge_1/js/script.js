21b041c78a2ea3bd8ff33967e8dd224e5c10cad4
Tim Durward <Timothy.A.Durward@gmail.com>
2015-05-02 21:40:22 -0700

    Added loops

---
+++ Javascript/Javascript Loops, Arrays and objects/Loops/Challenge_1/js/script.js
@@ -0,0 +1,57 @@
+
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
+
+function rgb(){
+red = Math.floor(Math.random() * 256 );
+green = Math.floor(Math.random() * 256 );
+blue = Math.floor(Math.random() * 256 );
+}	
+
+for(var i=1; i<=32; i+=1){
+rgb();
+rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+
+
+document.write(html);
+
+setTimeout(function () {
+       window.location.href = "index.html"; //will redirect to your blog page (an ex: blog.html)
+    }, 500); //will call the function after 2 secs.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
